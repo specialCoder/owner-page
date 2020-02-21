c5fb7ef3bdb11d8a84a11433acd77cd887cc2d69
zhubenlei <zhubenlei@bytedance.com>
2020-02-21 22:42:55 +0800

    样式修改

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import { StaticQuery,graphql } from "gatsby"
+import { StaticQuery,graphql } from "gatsby";
+import './style.css';
 
 function getSidebarLabels(data){
 	const sidebarItemsArray = [];
 	data.allSidebarItemsJson.edges.forEach((item)=>{
 		const {title='',link=''}=item.node;
-		const renderItem = <li><a href={link} key={link} target="_blank">{title}</a></li>
+		const renderItem = <li><a href={link} key={link} target="_blank" rel="noopener noreferrer">{title}</a></li>
 		sidebarItemsArray.push(renderItem);
 	});	
 	return sidebarItemsArray;
 }	
+
+const ListContent = (props)=>{
+	const {data} = props;
+	console.log('props data-->',props.data);
+
+	const handleClick = (url)=>{
+		window.open(url);
+	}
+
+	const renderItem = (item={},i)=>{
+		const {title,desc,link} = item;
+		return <div 
+		    key={i}
+			  className="list-item"
+			  onClick={handleClick}
+		  >
+			<p className="list-item-title">{title}</p>
+			<p className="list-item-desc">{desc}</p>
+		</div>
+	}
+
+	return <div className="list-content">
+			{data.map((item,i)=>{
+				return renderItem(item.node,i);
+			})}
+	</div>
+}
 
 const SideBar = ({children})=>{
 	return <StaticQuery 
@@ -27,9 +55,7 @@ const SideBar = ({children})=>{
 			}
 		`}
 		render={(data)=>{
-			return(<>
-				<ul>{getSidebarLabels(data)}</ul>
-			</>)
+			return <ListContent data={data.allSidebarItemsJson.edges}/>
 		}}
 		/>
 }
